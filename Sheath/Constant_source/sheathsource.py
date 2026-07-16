3d30fe38fa9f64abe0af6c910a847ac1d987e986
Ethan Webb <ewebb2@ufl.edu>
2026-07-16 15:59:55 -0400

    sheathsource is cleaned up. Adding the scipy modifications files for SSB

--- Sheath/Constant_source/sheathsource.py
+++ Sheath/Constant_source/sheathsource.py
@@ -82,42 +82,54 @@ def output_transform(inputs, outputs):
     
     return torch.cat((phiFinal,niFinal), dim=1)
 
+#PDE Definition
 def pde(inputs, outputs):
     phi, ni = outputs[:, 0:1], outputs[:, 1:2]
 
+    #Unfolding the network inputs
+    #They are currently scaled to [0,1] for the network
     xNorm, MiMeNorm, TiTeNorm,KnudNorm = inputs[:, 0:1], inputs[:,1:2], inputs[:,2:3], inputs[:,3:4]
 
+    #Scaling the inputs back to their non-dimensional values, which the PDE expects
     x = xMin + ( xMax - xMin ) * xNorm
     MiMe = MiMeMin + (MiMeMax - MiMeMin) * MiMeNorm
     TiTe = TiTeMin + (TiTeMax - TiTeMin) * TiTeNorm
     Knud = KnudMin + (KnudMax - KnudMin) * KnudNorm
 
-    Source = 1/L
-    Source_int = x/L
-    FluxAtWall = 1
-    uewall = torch.sqrt(MiMe/(2*np.pi))
-    ne = FluxAtWall/uewall*torch.exp(phi)
-    ui = Source_int / ni
+    #Defining constants in the PDE
+    Source = 1/L # constant source
+    Source_int = x/L #integral of continuity equation n*u = x/L
+    FluxAtWall = 1 #when x=1, n*u = 1
+    uewall = torch.sqrt(MiMe/(2*np.pi)) #Velocity at wall from random flux
+    ne = FluxAtWall/uewall*torch.exp(phi) #electron density at wall
+    ui = Source_int / ni #ambipolarity condition ui = ne*ue / ni = x/(L*ni)
     
+    #Auto-grad for the derivatives | one 'x' is a first derivative, two 'x's is a second derivative
+    #Derivatives need to be scaled since the network's inputs are scaled to [0,1]
+    #d/dx = d/dxNorm * dxNorm/dx = d/dxNorm * 1/(xMax-xMin)
     dphi_x = dde.grad.jacobian(outputs, inputs, i=0, j=0) / (xMax-xMin)
     dphi_xx = dde.grad.hessian(outputs, inputs, component=0, i=0, j=0) / (xMax-xMin)**2
     dni_x = dde.grad.jacobian(outputs, inputs, i=1, j=0) / (xMax-xMin)
     dui_x = dde.grad.jacobian(ui, inputs, i=0, j=0) / (xMax-xMin)
     
+    #Physics Equations
     lossb1 = dphi_xx - (ne-ni) # Poisson
     lossb2 = L*(TiTe*dni_x + Z*ni*dphi_x + ui*Source + Source_int*dui_x + Source_int*Knud)#ion momentum
 
     return lossb1,lossb2
 
 def main():
+    #set 4D hypercube for inputs scaled to [0,1]
     geom = dde.geometry.Hypercube([0,0,0,0], [1,1,1,1])
 
+    #Set up the neural network architecture and apply input (feature) and output transforms
     net = dde.maps.FNN([4] + [32]*4 + [2], 'tanh', "Glorot normal")
     net.apply_feature_transform(feature_transform)
     net.apply_output_transform(output_transform)
 
     losses = []
 
+    #Pass the network structure, objective function, and point sampling to the data object
     data = dde.data.PDE(
         geom,
         pde,
@@ -129,13 +141,17 @@ def main():
         test_distribution='Sobol',
     )
 
+    #build model
     model = dde.Model(data, net)
 
+    #Set up a callback to resample the PDE points every 500 iterations
     PDE_Resampler = dde.callbacks.PDEPointResampler(period=500,pde_points=True)
 
+    #Manual weights for loss terms if desired
     loss_weights = [1,1]
     loss = ["MSE"] * 2
 
+    #Compile and train model using ADAM
     model.compile("adam", lr=lr, loss=loss, loss_weights=loss_weights)
     losshistory, train_state = model.train(epochs=0,callbacks=[PDE_Resampler], model_save_path = save_path + 'model.pt')
 
@@ -145,6 +161,7 @@ def main():
     save_solution(geom, model, "solution0")
     dde.saveplot(losshistory, train_state, issave=True, isplot=True,output_dir= save_path + 'data')
 
+    #Complie and train using SSBroyden
     for i in range(0,NumBFGS):
         model.compile("SSBroyden")
         losshistory, train_state = model.train(display_every=1000,callbacks=[PDE_Resampler],model_save_path = save_path + 'model.pt')
